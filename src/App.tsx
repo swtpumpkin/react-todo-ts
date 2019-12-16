ff50fdd92e79eba16ae944927f473053036abbf5
swtpumpkin <swtpumpkin90@gmail.com>
2019-12-16 10:47:40 +0900

    Add fontawesome and table list

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,58 @@
-import React, {ChangeEvent} from 'react';
+import React, { ChangeEvent } from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faTrash);
 
 interface Item {
     name: string,
-    key: number | null,
+    key: number,
 }
 
 const App: React.FC = () => {
-    const [state, setState] = React.useState<Item>({name: '', key: null});
+    const [state, setState] = React.useState<Item>({name: '', key: 0});
     const [items, setItems] = React.useState<Item[]>([]);
 
     const onChangeTodo = (e: ChangeEvent<HTMLInputElement>) => {
         setState({
             name: e.target.value,
-            key: Date.now()
+            key: 0
         })
+    };
+
+    const deleteItem = (item: Item) => {
+        setItems(items.filter(obj => obj.key !== item.key))
     };
 
     return (
         <div className="App">
-            <form id="to-do-form">
+            <form id="to-do-form" onSubmit={e => {
+                e.preventDefault();
+                // 빈 값 이벤트 끊기
+                if (!state.name) return;
+                setItems([...items, {
+                    ...state,
+                    key: Date.now()
+                }]);
+            }}>
                 <input type="text"
                        placeholder="Enter Text"
                        value={state.name}
                        onChange={onChangeTodo}
                 />
-                <button onClick={e => {
-                    e.preventDefault();
-                    setItems([...items, state])
-                }}>
+                <button type="submit">
                     Add
                 </button>
             </form>
             <div id="to-do-list">
-                <ul>
+                <ul className="item-list">
+                    {items.map((item) => <li className="item" key={item.key}><p>{item.name}</p><span
+                        onClick={() => deleteItem(item)}>
+                        <FontAwesomeIcon icon="trash"/>
+                    </span>
+                    </li>)}
                 </ul>
             </div>
         </div>
